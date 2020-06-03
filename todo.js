a43a1c018da9ac9568968f5897cabd1e5b22597e
Dok1211 <wataru.douke@gmail.com>
2020-06-03 12:30:00 +0900

    指摘箇所修正

--- todo.js
+++ todo.js
@@ -1,26 +1,38 @@
 const addTaskButton = document.querySelector('#addTaskButton');
 const taskText = document.querySelector('#taskText')
 const todoList = document.querySelector('#todoList');
-const addTask = task => {
-    const todo = document.getElementsByClassName('todo');
-    const id = todo !== undefined ? todo.length : 0;
-    const html =
-        `<tr class="todo">
-            <td>${id}</td>
-            <td>${task}</td>
-            <td>
-                <input type="submit" value="作業中">
-            </td>
-            <td>
-                <input type="submit" value="削除">
-            </td>
-        </tr>`;
-    todoList.innerHTML += html;
+let todos = [];
+const displayTask = (task, todos) => {
+    const status = {0: 'すべて', 1: '作業中', 2: '完了'};
+    const tr = document.createElement('tr');
+    const tdId = document.createElement('td');
+    const tdTask = document.createElement('td');
+    const tdStatus = document.createElement('td');
+    const buttonStatus = document.createElement('input');
+    const buttonDelete = document.createElement('input');
+    const tdDelete = document.createElement('td');
+    todos.push({comment: task, status: status[1]});
+    todos.forEach((element, index) => {
+        tr.className = 'todo';
+        tdId.textContent = String(index);
+        tdTask.textContent = element.comment;
+        buttonStatus.setAttribute('type', 'submit');
+        buttonStatus.setAttribute('value', element.status);
+        buttonDelete.setAttribute('type', 'submit');
+        buttonDelete.setAttribute('value', '削除');
+        tdStatus.appendChild(buttonStatus);
+        tdDelete.appendChild(buttonDelete);
+        tr.appendChild(tdId);
+        tr.appendChild(tdTask);
+        tr.appendChild(tdStatus);
+        tr.appendChild(tdDelete);
+        todoList.appendChild(tr);
+    });
 };
-addTaskButton.addEventListener('click', e => {
+addTaskButton.addEventListener('click', {array: todos, handleEvent(evt) {
     const task = taskText.value;
     if (task.length) {
-        addTask(task);
+        displayTask(task, todos);
         taskText.value = '';
     }
-});
+}});
