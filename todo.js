4380fc25c80eb50a5eaf11405e49f50b988b1f4c
Dok1211 <wataru.douke@gmail.com>
2020-06-04 15:17:24 +0900

    commit取り消し

--- todo.js
+++ todo.js
@@ -2,11 +2,6 @@ const addTaskButton = document.querySelector('#addTaskButton');
 const taskText = document.querySelector('#taskText')
 const todoList = document.querySelector('#todoList');
 let todos = [];
-const resetIds = () => {
-    document.querySelectorAll('.id').forEach((td, index) => {
-        td.textContent = String(index);
-    });
-};
 const displayTask = (task, todos) => {
     const status = {0: 'すべて', 1: '作業中', 2: '完了'};
     const tr = document.createElement('tr');
@@ -17,9 +12,9 @@ const displayTask = (task, todos) => {
     const buttonDelete = document.createElement('input');
     const tdDelete = document.createElement('td');
     todos.push({comment: task, status: status[1]});
-    todos.forEach(element => {
+    todos.forEach((element, index) => {
         tr.className = 'todo';
-        tdId.className = 'id';
+        tdId.textContent = String(index);
         tdTask.textContent = element.comment;
         buttonStatus.setAttribute('type', 'submit');
         buttonStatus.setAttribute('value', element.status);
@@ -32,12 +27,6 @@ const displayTask = (task, todos) => {
         tr.appendChild(tdStatus);
         tr.appendChild(tdDelete);
         todoList.appendChild(tr);
-    });
-    resetIds();
-    buttonDelete.addEventListener('click', (evt) => {
-        todos.splice(todos.length, 1);
-        tr.parentNode.removeChild(tr);
-        resetIds();
     });
 };
 addTaskButton.addEventListener('click', {array: todos, handleEvent(evt) {
