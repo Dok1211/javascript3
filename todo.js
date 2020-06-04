5d39cbe562d3318b4233ef4150e6d5d48d82cd0f
Dok1211 <wataru.douke@gmail.com>
2020-06-04 23:55:59 +0900

    タスク状態、表示切り替え

--- todo.js
+++ todo.js
@@ -1,6 +1,8 @@
 const addTaskButton = document.querySelector('#addTaskButton');
 const taskText = document.querySelector('#taskText')
 const todoList = document.querySelector('#todoList');
+const statusRadioButton = document.getElementsByName('status');
+const status = {0: 'すべて', 1: '作業中', 2: '完了'};
 let todos = [];
 const resetIds = () => {
     document.querySelectorAll('.id').forEach((td, index) => {
@@ -8,7 +10,6 @@ const resetIds = () => {
     });
 };
 const displayTask = (task, todos) => {
-    const status = {0: 'すべて', 1: '作業中', 2: '完了'};
     const tr = document.createElement('tr');
     const tdId = document.createElement('td');
     const tdTask = document.createElement('td');
@@ -23,6 +24,7 @@ const displayTask = (task, todos) => {
         tdTask.textContent = element.comment;
         buttonStatus.setAttribute('type', 'submit');
         buttonStatus.setAttribute('value', element.status);
+        buttonStatus.setAttribute('data-status-num', '1');
         buttonDelete.setAttribute('type', 'submit');
         buttonDelete.setAttribute('value', '削除');
         tdStatus.appendChild(buttonStatus);
@@ -39,11 +41,48 @@ const displayTask = (task, todos) => {
         tr.parentNode.removeChild(tr);
         resetIds();
     });
+    buttonStatus.addEventListener('click', (evt) => {
+        switch (buttonStatus.value) {
+            case status[1]:
+                buttonStatus.value = status[2];
+                buttonStatus.dataset.statusNum = '2';
+                break;
+            case status[2]:
+                buttonStatus.value = status[1];
+                buttonStatus.dataset.statusNum = '1';
+                break;
+        }
+    });
 };
-addTaskButton.addEventListener('click', {array: todos, handleEvent(evt) {
+addTaskButton.addEventListener('click', (evt) => {
     const task = taskText.value;
     if (task.length) {
         displayTask(task, todos);
         taskText.value = '';
     }
-}});
+});
+statusRadioButton.forEach(e => {
+    e.addEventListener('click', evt => {
+        document.querySelectorAll('.todo').forEach(e => {
+            e.style.display = 'table-row';
+        })
+        switch (e.dataset.statusNum) {
+            case '0':
+                break;
+            case '1':
+                if (todoList.querySelectorAll('[data-status-num="2"]').length) {
+                    todoList.querySelectorAll('[data-status-num="2"]').forEach(e => {
+                        e.parentNode.parentNode.style.display = 'none';
+                    });
+                }
+                break;
+            case '2':
+                if (todoList.querySelectorAll('[data-status-num="1"]').length) {
+                    todoList.querySelectorAll('[data-status-num="1"]').forEach(e => {
+                        e.parentNode.parentNode.style.display = 'none';
+                    });
+                }
+                break;
+        }
+    });
+});
